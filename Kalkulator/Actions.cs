50476f3d72a20953ec48613b5bf16185e9d965bb
Ivan <brandy.bg@mail.ru>
2018-11-30 23:29:13 +0300

    Не уверен, что в абстрактном классе то, что надо, но он естьgit commit -m !

---
+++ Kalkulator/Actions.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kalkulator
+{
+
+    internal abstract class Actions : Matrix
+    {
+        public Actions(int countOfColumn, int countOfRow) : base(countOfColumn, countOfRow)
+        {
+        }
+
+        public static int row;
+        public static int column;
+        public static Matrix matrixOne;
+        public static Matrix matrixTwo;
+
+        public static void ShowMatrix(Matrix matrix)
+        {
+            for (int i = 0; i < matrix.Value.Length; i++)
+            {
+                for (int j = 0; j < matrix.Value[i].Length; j++)
+                {
+                    Console.Write(string.Format("{0:F1}", matrix.Value[i][j]) + "\t");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        private static void InitializerVariables()
+        {
+            column = SetVariableSize("колонок");
+            row = SetVariableSize("строк");
+        }
+
+        public static int SetVariableSize(string variableName)
+        {
+            int variable = 0;
+            Console.Write($"Введите количество {variableName}: ");
+            variable = int.Parse(Console.ReadLine());
+            return variable;
+        }       
+
+        public static Matrix CreateMatrix()
+        {
+            return new Matrix(column, row);
+        }
+
+        public static Matrix AddMatrix()
+        {
+            InitializerVariables();
+            Matrix matrix = CreateMatrix();
+            Console.WriteLine("Задайте значения матрицы:");
+            for (int i = 0; i < matrix.Value.Length; i++)
+            {
+                for (int j = 0; j < matrix.Value[i].Length; j++)
+                {
+                    Console.Write("Число: ");
+                    matrix.Value[i][j] = decimal.Parse(Console.ReadLine());
+                }
+            }
+            return matrix;
+        }
+
+        public static void AdditionTwoMatrix()
+        {
+            matrixOne = AddMatrix();
+            Console.WriteLine("Матрица 1: ");
+            ShowMatrix(matrixOne);
+            matrixTwo = AddMatrix();
+            Console.WriteLine("Матрица 2: ");
+            ShowMatrix(matrixTwo);
+        }
+
+
+    }
+}
---
+++ Kalkulator/InitializerMatrix.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kalkulator
+{
+    
+
+    internal class InitializerMatrix : Actions
+    {       
+        public InitializerMatrix(int countOfColumn, int countOfRow) : base(countOfColumn, countOfRow)
+        {
+        }
+        
+        public static void Choice()
+        {
+            Console.Write("Введите действие: ");
+            string selection = Console.ReadLine();
+            switch (selection)
+            {                
+                case "+":
+                    AdditionTwoMatrix();
+                    Matrix matrixSum = matrixOne + matrixTwo;
+                    Console.WriteLine("Сумма первой и второй матрицы: ");
+                    ShowMatrix(matrixSum);
+                    break;
+
+                case "*":
+                    AdditionTwoMatrix();
+                    Matrix matrixMult = matrixOne * matrixTwo;
+                    Console.WriteLine("Умножение первой и второй матрицы равно: ");
+                    ShowMatrix(matrixMult);
+                    break;
+
+                case "-":
+                    AdditionTwoMatrix();
+                    Matrix matrixDifference = matrixOne - matrixTwo;
+                    Console.WriteLine("Разность первой и второй матрицы: ");
+                    ShowMatrix(matrixDifference);
+                    break;
+
+                case "/":
+                    AdditionTwoMatrix();
+                    Matrix matrixShare = matrixOne / matrixTwo;
+                    Console.WriteLine("Деление первой и второй матрицы: ");
+                    ShowMatrix(matrixShare);
+                    break;
+
+                case "T":
+                    matrixOne = AddMatrix();
+                    Console.WriteLine("Матрица: ");
+                    ShowMatrix(matrixOne);
+                    Console.WriteLine("Транспорированая матрица");
+                    Matrix matrixTrans = matrixOne.Transport();
+                    ShowMatrix(matrixTrans);
+                    break;
+                default:
+                    Console.WriteLine("Не корректный ввод");
+                    break;
+
+
+            }
+        }
+
+
+    }
+}
